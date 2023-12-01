a4f751dce6326cf59dfaa49ca59a03e66b2a8301
Викторина <semenov_ed2020isp@mail.ru>
2023-12-01 22:44:53 +0500

    ррропрпп

---
+++ app/src/main/java/com/example/quizapp/playActivity.java
@@ -0,0 +1,103 @@
+package com.example.quizapp;
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+public class playActivity extends AppCompatActivity {
+    String[] question_list = {"Как называют людей, идущих по улице?",
+            "Где должны ездить машины?",
+            "Как называется элемент дороги, предназначенный для движения пешеходов:",
+            "Где разрешается переходить улицу пешеходам?",
+            "Как нужно переходить улицу?",
+
+    };
+    String[] choose_list = {"пешеходы","водители","дети",
+            "обочина","по тротуару","везде",
+            "по дороге","обочина", "обочина","полоса движения",
+            "тротуар","на светофоре или по пешеходному переходу","нигде",
+            "там, где удобно пешеходу","на светофоре или по пешеходному переходу","медленно идти при этом петь песни",
+            "нужно быстро бежать", "спокойным, твердым шагом, держа взрослого за руку", "медленно идти при этом петь песни",
+
+    };
+    String[] correct_list = {"пешеходы", "по дороге", "тротуар", "на светофоре или по пешеходному переходу", "спокойным, твердым шагом, держа взрослого за руку"};
+    TextView cpt_question, text_question;
+    Button btn_choose1, btn_choose2, btn_choose3, btn_next;
+    int currentQuestion = 0;
+    int scorePlayer = 0;
+    boolean isclickBtn = false;
+    String valueChoose = "";
+    Button btn_click;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_play);
+        cpt_question = findViewById(R.id.cpt_question);
+        text_question = findViewById(R.id.text_question);
+        btn_choose1 = findViewById(R.id.btn_choose1);
+        btn_choose2 = findViewById(R.id.btn_choose2);
+        btn_choose3 = findViewById(R.id.btn_choose3);
+        btn_next = findViewById(R.id.btn_next);
+        findViewById(R.id.image_back).setOnClickListener(
+                a-> finish()
+        );
+        remplirData();
+        btn_next.setOnClickListener(
+                view -> {
+                        if (isclickBtn){
+                            isclickBtn = false;
+                            if(!valueChoose.equals(correct_list[currentQuestion])){
+                                Toast.makeText(playActivity.this , "Не првильно",Toast.LENGTH_LONG).show();
+                                btn_click.setBackgroundResource(R.drawable.background_btn_erreur);
+                            }else {
+                                Toast.makeText(playActivity.this , "Правильно",Toast.LENGTH_LONG).show();
+                                btn_click.setBackgroundResource(R.drawable.background_btn_correct);
+                                scorePlayer++;
+                            }
+                            new Handler().postDelayed(() -> {
+                                if(currentQuestion!=question_list.length-1){
+                                    currentQuestion = currentQuestion + 1;
+                                    remplirData();
+                                    valueChoose = "";
+                                    btn_choose1.setBackgroundResource(R.drawable.background_btn_choose);
+                                    btn_choose2.setBackgroundResource(R.drawable.background_btn_choose);
+                                    btn_choose3.setBackgroundResource(R.drawable.background_btn_choose);
+                                }else {
+                                    Intent intent = new Intent(playActivity.this, ResulteActivity.class);
+                                    intent.putExtra("Resute", scorePlayer);
+                                    startActivity(intent);
+                                    finish();
+                                }
+                            },2000);
+
+                        }else {
+                            Toast.makeText(playActivity.this ,  "Вы должны выбрать один из них",Toast.LENGTH_LONG).show();
+                        }
+                }
+        );
+    }
+    void remplirData(){
+        cpt_question.setText((currentQuestion+1) + "/" + question_list.length);
+        text_question.setText(question_list[currentQuestion]);
+        btn_choose1.setText(choose_list[4 * currentQuestion]);
+        btn_choose2.setText(choose_list[4 * currentQuestion+1]);
+        btn_choose3.setText(choose_list[4 * currentQuestion+2]);
+    }
+    public void ClickChoose(View view) {
+        btn_click = (Button)view;
+        if (isclickBtn) {
+            btn_choose1.setBackgroundResource(R.drawable.background_btn_choose);
+            btn_choose2.setBackgroundResource(R.drawable.background_btn_choose);
+            btn_choose3.setBackgroundResource(R.drawable.background_btn_choose);
+        }
+        chooseBtn();
+    }
+    void chooseBtn(){
+        btn_click.setBackgroundResource(R.drawable.background_btn_choose_color);
+        isclickBtn = true;
+        valueChoose = btn_click.getText().toString();
+    }
+}
